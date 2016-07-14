befb3adb3b6ee71f1fc6bb5f8c35c20bd1bf9a7d
ygtzz <ygtzz@126.com>
2016-07-14 14:20:48 +0800

    add react router

--- pages/index/index.jsx
+++ pages/index/index.jsx
@@ -2,10 +2,9 @@ var React = require('react');
 var ReactDom = require('react-dom');
 var Trend = require('pages/trend/trend.jsx');
 var Article = require('pages/article/article.jsx');
-var Router = require('director').Router;
 var PubSub = require('pubsub');
 var oEventType = require('eventType/eventType');
-var router = new Router();
+var Router = require('react-router');
 
 var App = React.createClass({
 	getInitialState: function() {
@@ -66,51 +65,51 @@ ReactDom.render(
 );
 
 //首页
-router.on('/home', function (cate){
-    PubSub.publish(oEventType.currentView,'home');
-})
-//热门文章
-router.on('/hot/:cate', function (cate){
-    listHandler('trend','hot',cate);
-})
-//分类推荐
-router.on('/notes/:cate', function (cate){
-    listHandler('trend','notes',cate);
-})
-//我的订阅
-router.on('/subscribe/:cate', function (cate){
-    listHandler('trend','subscribe',cate);
-})
-//文章详细
-router.on('/p/:id', function (id){
-	var self = this;
-    require.async('pages/article/article.jsx', function(pageComponent){
-    	PubSub.publish(oEventType.currentView,'article');
-    	PubSub.publish(oEventType.articleId, id);
-    });
-})
-/*错误页*/
-router.on('/error/notfound', function () {
-	var self = this;
-    require.async('pages/error/notfound.jsx', function(pageComponent){
-    	PubSub.publish(oEventType.currentView,'not-found');
-    });
-})
-//页面未找到
-router.configure({
-  notfound: function () {
-    router.setRoute('/error/notfound');
-  }
-})
-//默认首页
-router.init('/hot/now');
+// router.on('/home', function (cate){
+//     PubSub.publish(oEventType.currentView,'home');
+// })
+// //热门文章
+// router.on('/hot/:cate', function (cate){
+//     listHandler('trend','hot',cate);
+// })
+// //分类推荐
+// router.on('/notes/:cate', function (cate){
+//     listHandler('trend','notes',cate);
+// })
+// //我的订阅
+// router.on('/subscribe/:cate', function (cate){
+//     listHandler('trend','subscribe',cate);
+// })
+// //文章详细
+// router.on('/p/:id', function (id){
+// 	var self = this;
+//     require.async('pages/article/article.jsx', function(pageComponent){
+//     	PubSub.publish(oEventType.currentView,'article');
+//     	PubSub.publish(oEventType.articleId, id);
+//     });
+// })
+// /*错误页*/
+// router.on('/error/notfound', function () {
+// 	var self = this;
+//     require.async('pages/error/notfound.jsx', function(pageComponent){
+//     	PubSub.publish(oEventType.currentView,'not-found');
+//     });
+// })
+// //页面未找到
+// router.configure({
+//   notfound: function () {
+//     router.setRoute('/error/notfound');
+//   }
+// })
+// //默认首页
+// router.init('/hot/now');
 
-//文章列表页通用处理
-function listHandler(view,type,cate){
-    require.async('pages/trend/trend.jsx', function(pageComponent){
-    	PubSub.publish(oEventType.type,type);
-    	PubSub.publish(oEventType.cate,cate);
-    	PubSub.publish(oEventType.currentView,view);
-    });
-}
+// //文章列表页通用处理
+// function listHandler(view,type,cate){
+//     require.async('pages/trend/trend.jsx', function(pageComponent){
+//     	PubSub.publish(oEventType.type,type);
+//     	PubSub.publish(oEventType.cate,cate);
+//     	PubSub.publish(oEventType.currentView,view);
+//     });
+// }
 
